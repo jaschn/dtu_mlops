ffe1025739d8eab1981c33dc4d700c793dbb9f24
jaschn <49870201+jaschn@users.noreply.github.com>
2022-01-04 17:35:57 +0100

    Exercises of the first day

--- s1_getting_started/exercise_files/final_exercise/data.py
+++ s1_getting_started/exercise_files/final_exercise/data.py
@@ -1,8 +1,43 @@
 import torch
+import numpy as np
+from torch.utils.data import Dataset, DataLoader
+import os
 
+class CorruptMNISTset(Dataset):
+    def __init__(self, type="train"):
+        base_path = "./../../../data/corruptmnist"
+        if type == "train":
+            with np.load("./../../../data/corruptmnist/train_0.npz") as f:
+                im = torch.from_numpy(f["images"]).type(torch.float)
+                self.images = im.view(im.size(0), 1, im.size(1), im.size(2))
+                self.labels = torch.from_numpy(f["labels"]).type(torch.long)
+            for i in range(1, 5):
+                with np.load(os.path.join(base_path, f"train_{i}.npz")) as f:
+                    im = torch.from_numpy(f["images"]).type(torch.float)
+                    im = im.view(im.size(0), 1, im.size(1), im.size(2))
+                    self.images = torch.cat([self.images, im], dim=0)        
+                    labels = torch.from_numpy(f["labels"]).type(torch.long)
+                    self.labels = torch.cat([self.labels, labels], dim=0)
+        elif type == "test":
+            with np.load("./../../../data/corruptmnist/test.npz") as f:
+                im = torch.from_numpy(f["images"]).type(torch.float)
+                self.images = im.view(im.size(0), 1, im.size(1), im.size(2))                
+                self.labels = torch.from_numpy(f["labels"]).type(torch.long)
+        else:
+            assert "wrong option"
+
+        assert len(self.images) == len(self.labels), "image count does not match label count"
+
+
+    def __len__(self):
+        return len(self.images)
+
+    def __getitem__(self, index):
+        return self.images[index], self.labels[index]
 
 def mnist():
-    # exchange with the corrupted mnist dataset
-    train = torch.randn(50000, 784)
-    test = torch.randn(10000, 784) 
-    return train, test
+    trainset = CorruptMNISTset(type="train")
+    testset = CorruptMNISTset(type="test")
+    trainloader = DataLoader(trainset, batch_size=64, shuffle=True, num_workers=8)
+    testloader = DataLoader(testset, batch_size=64, shuffle=True, num_workers=8)
+    return trainloader, testloader 
--- s1_getting_started/exercise_files/final_exercise/main.py
+++ s1_getting_started/exercise_files/final_exercise/main.py
@@ -2,6 +2,8 @@ import argparse
 import sys
 
 import torch
+from torch import nn, optim
+import matplotlib.pyplot as plt
 
 from data import mnist
 from model import MyAwesomeModel
@@ -29,14 +31,35 @@ class TrainOREvaluate(object):
     def train(self):
         print("Training day and night")
         parser = argparse.ArgumentParser(description='Training arguments')
-        parser.add_argument('--lr', default=0.1)
+        parser.add_argument('--lr', default=0.001)
+        parser.add_argument('--epochs', default=10)
         # add any additional argument that you want
         args = parser.parse_args(sys.argv[2:])
         print(args)
         
         # TODO: Implement training loop here
         model = MyAwesomeModel()
+        critirion = nn.NLLLoss()
+        optimizer = optim.Adam(model.parameters(), lr=args.lr)
         train_set, _ = mnist()
+        losses = []
+        for e in range(args.epochs):
+            running_loss = 0
+            for images, labels in train_set:
+                out = model(images)
+                loss = critirion(out, labels)
+                loss.backward()
+                optimizer.step()
+                optimizer.zero_grad()
+                running_loss += loss.item()
+            losses.append(running_loss)
+        plt.plot(losses, label="Loss")
+        plt.legend()
+        plt.title("Loss")
+        plt.savefig("loss.png")
+        torch.save(model.state_dict(), "trained_model.pt")
+
+
         
     def evaluate(self):
         print("Evaluating until hitting the ceiling")
@@ -47,8 +70,20 @@ class TrainOREvaluate(object):
         print(args)
         
         # TODO: Implement evaluation logic here
-        model = torch.load(args.load_model_from)
+        model_states = torch.load(args.load_model_from)
+        model = MyAwesomeModel()
+        model.load_state_dict(model_states)
         _, test_set = mnist()
+        running_acc = []
+        with torch.no_grad():
+            for image, label in test_set:
+                out = model(image)
+                ps = out.exp()
+                _, top_class = ps.topk(1, dim=1)
+                equals = top_class == label.view(*top_class.shape)
+                running_acc.append(equals)
+        accuracy = torch.mean(torch.cat(running_acc).type(torch.FloatTensor))
+        print(f'Accuracy: {accuracy.item()*100}%')
 
 if __name__ == '__main__':
     TrainOREvaluate()
